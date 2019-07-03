8afdd0656908cc3a49b0cf93e90c61d7618c8346
dpanaben <dpan.orz+github@gmail.com>
2019-07-03 11:23:01 +0800

    Update config.php

--- user/config.php
+++ user/config.php
@@ -69,7 +69,7 @@ define( 'YOURLS_COOKIEKEY', 'modify this text with something random' );
  ** YOURLS will auto encrypt plain text passwords in this file
  ** Read http://yourls.org/userpassword for more information */
 $yourls_user_passwords = array(
-	'admin' => 'admin',
+	getenv("ADMIN_ID") => getenv("ADMIN_PW"),
 	// 'username2' => 'password2',
 	// You can have one or more 'login'=>'password' lines
 	);
